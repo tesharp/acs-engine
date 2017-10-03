17d7ae0125fb66d9d12b19b3a38b299731fc5491
CecileRobertMichon <cerobert@microsoft.com>
2017-10-03 16:23:19 -0700

    wip upgrade test

--- cmd/upgrade_test.go
+++ cmd/upgrade_test.go
@@ -6,10 +6,26 @@ import (
 
 func TestUpgradeValidateCmd(t *testing.T) {
 
+	// deployCmd := &deployCmd{
+	// 	apimodelPath:    "./this/is/unused.json",
+	// 	dnsPrefix:       "dnsPrefix1",
+	// 	outputDirectory: "_test_output",
+	// 	location:        "westus",
+
+	// 	client: &armhelpers.MockACSEngineClient{},
+	// }
+
+	//autofillApimodel(deployCmd)
+
 	v := upgradeCmd{
-		resourceGroupName: "TestGroup",
-		upgradeModelFile:  "../pkg/acsengine/testdata/simple/kubernetes.json",
+		resourceGroupName:   "TestGroup",
+		upgradeModelFile:    "../pkg/acsengine/testdata/simple/kubernetes.json",
+		deploymentDirectory: "_test_output",
 	}
+	v.authArgs.RawAzureEnvironment = "AzurePublicCloud"
+	v.authArgs.AuthMethod = "device"
+	v.authArgs.rawSubscriptionID = "testID"
+
 	r := NewRootCmd()
 
 	err := v.validate(r, []string{})
