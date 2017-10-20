d6aba75ff831d8a19a3e00bf25ac18cab12c1012
Terje Torkelsen <terje@eye-share.no>
2017-10-20 22:05:49 +0200

    Changing to use if-up.d script as dhcp exit hook wouldn't work

--- extensions/register-dns/v1/register-dns.sh
+++ extensions/register-dns/v1/register-dns.sh
@@ -7,40 +7,33 @@ DOMAINNAME=$1
 
 echo $(date) " - Starting Script"
 
-cat > /etc/dhcp/dhclient-exit-hooks.d/register-dns <<EOFDHCP
+cat > /etc/network/if-up.d/register-dns <<EOFDHCP
 #!/bin/sh
 
 # only execute on the primary nic
-if [ "\$interface" != "eth0" ]
+if [ "\${IFACE}" = "eth0" ]
 then
-    return
-fi
-
-# when we have a new IP, perform nsupdate
-if [ "\$reason" = BOUND ] || [ "\$reason" = RENEW ] ||
-    [ "\$reason" = REBIND ] || [ "\$reason" = REBOOT ]
-then
+    ip=\$(ip address show eth0 | awk '/inet / {print \$2}' | cut -d/ -f1)
     host=\$(hostname -s)
     nsupdatecmds=/var/tmp/nsupdatecmds
-    echo "update delete \$host.${DOMAINNAME} a" > \$nsupdatecmds
-    echo "update add \$host.${DOMAINNAME} 3600 a \$new_ip_address" >> \$nsupdatecmds
+    echo "update delete \${host}.${DOMAINNAME} a" > \$nsupdatecmds
+    echo "update add \${host}.${DOMAINNAME} 3600 a \${ip}" >> \$nsupdatecmds
     echo "send" >> \$nsupdatecmds
 
-    sleep 10
     nsupdate \$nsupdatecmds
 fi
 EOFDHCP
 
-chmod 755 /etc/dhcp/dhclient-exit-hooks.d/register-dns
+chmod 755 /etc/network/if-up.d/register-dns
 
-if ! grep -Fq "$(DOMAINNAME)" /etc/dhcp/dhclient.conf
+if ! grep -Fq "${DOMAINNAME}" /etc/dhcp/dhclient.conf
 then
     echo $(date) " - Adding domain to dhclient.conf"
 
-    echo "supersede domain-name \"$(DOMAINNAME)\";" >> /etc/dhcp/dhclient.conf
-    echo "prepend domain-search \"$(DOMAINNAME)\";" >> /etc/dhcp/dhclient.conf
+    echo "supersede domain-name \"${DOMAINNAME}\";" >> /etc/dhcp/dhclient.conf
+    echo "prepend domain-search \"${DOMAINNAME}\";" >> /etc/dhcp/dhclient.conf
 fi
 
 # service networking restart
 echo $(date) " - Restarting network"
-/etc/init.d/networking restart
+sudo ifdown eth0 && sudo ifup eth0
